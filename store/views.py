301b524b56ccce0e6f673e37705c4e331c9bd252
Bishnu <thapabishnu20@gmail.com>
2022-08-11 11:28:20 +0545

    cart functionality added

--- store/views.py
+++ store/views.py
@@ -3,7 +3,8 @@ from itertools import product
 from logging import exception
 from unicodedata import category
 from django.shortcuts import render, get_object_or_404
-
+from carts.models import CartItem
+from carts.views import _cart_id
 from category.models import Category
 from .models import Product
 # Create your views here.
@@ -32,10 +33,15 @@ def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(
             category__slug=category_slug, slug=product_slug)
+
+        # checks if the product is already exists in the cart
+        in_cart = CartItem.objects.filter(
+            cart__cart_id=_cart_id(request), product=single_product).exists()
     except exception as e:
         raise e
 
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
